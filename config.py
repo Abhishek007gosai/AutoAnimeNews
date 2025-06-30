581acc3724a7cd01534df0c998bb93150cd72c31
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-06-30 17:01:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 API_ID = "29245477" #API ID get it from telegram.org
 API_HASH = "0abc83883262245c90ca337b7a0375c4" #API HASH get it from telegram.org
-BOT_TOKEN = "7831174760:AAFKWWGJA2McsmSlrZUnYvXfy_BIvMsPf70" #Bot Token get it from @BotFather
+BOT_TOKEN = ":" #Bot Token get it from @BotFather
 URL_A = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "https://litter.catbox.moe/m4c4wu.jpg" #Startup Pic [Optional]
