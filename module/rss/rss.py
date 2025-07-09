51bf9af1e59e2e963593bcacadec00a7473c1398
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-10 02:12:30 +0530

    Update rss.py

--- module/rss/rss.py
+++ module/rss/rss.py
@@ -147,6 +147,6 @@ async def format_rss_entry(entry):
     msg = (
         f"<b><blockquote>{title}</blockquote></b>\n"
         f"<b><blockquote expandable><i>{summary}</i></blockquote expandable>\n"
-        f"<b><blockquote><a href='{link}'>Source</a></blockquote>\n»@Anime_News_Arena</b>"
+        f"<b><blockquote><a href='{link}'>────「  📖 SOURCE  」</a></blockquote>\n» @Anime_News_Arena</b>"
     )
     return msg, thumbnail_url, link
